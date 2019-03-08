fcedc56aaecb4d5d42f5fb7facf3793d817641d5
FernandoMedin <fernando-medin@outlook.com>
2019-03-08 12:15:57 -0300

    Add get pets test

---
+++ pets/tests/pets_test.py
@@ -0,0 +1,18 @@
+import grpc
+import os
+import sys
+
+sys.path.append("..")
+
+import pets_pb2 as p
+import pets_pb2_grpc as p_grpc
+
+
+def test_get_pets():
+    with grpc.insecure_channel(os.environ['PETS_HOST']) as channel:
+        stub = p_grpc.PetsStub(channel)
+        response = stub.get_pets(p.pets_request(user_id=1))
+        assert response.pets_message.lower() == "your pets: milu, bali"
+
+if __name__ == "__main__":
+    test_get_pets()
